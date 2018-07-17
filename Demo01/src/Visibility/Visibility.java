d53d126a9e0be861a6d43ed8c3242fcd00f19c8a
Sumathi Thirumugam <Sumathi_Thirumugam@ad.infosys.com>
2018-07-17 17:38:04 +0530

    'added'

--- Demo01/src/Visibility/Visibility.java
+++ Demo01/src/Visibility/Visibility.java
@@ -1,24 +1,33 @@
 package Visibility;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 //import org.openqa.selenium.ie.InternetExplorerDriver;
 public class Visibility {
 
        /**
         * @param args
+     * @throws MalformedURLException 
         */
 
-       public static void main(String[] args) {
+       public static void main(String[] args) throws MalformedURLException {
 
               // objects and variables instantiation
-    	       // System.setProperty("webdriver.firefox.marionette","D:\\siva\\eclipse\\workspace\\Demo01\\drivers\\geckodriver.exe");
-    	   System.setProperty("webdriver.chrome.driver", "/home/ec2-user/SELENIUMDEMO/Demo01/drivers/chromedriver.exe");
-              WebDriver driver = new ChromeDriver();
+                       // System.setProperty("webdriver.firefox.marionette","D:\\siva\\eclipse\\workspace\\Demo01\\drivers\\geckodriver.exe");
+                   DesiredCapabilities chromeCapabilities = DesiredCapabilities.chrome();
+                   System.setProperty("webdriver.chrome.driver", "/home/ec2-user/SELENIUMDEMO/Demo01/drivers/chromedriver.exe");
+                   WebDriver driver = new RemoteWebDriver(new URL("http://52.15.40.24:4455/wd/hub"), chromeCapabilities);
+              
+              
               String appUrl = "https://google.com";
 
               // launch the firefox browser and open the application url
